f0680f35d6c1053744b7ffd374f75d94bd4777da
Tushar Arora <personal.tushararora31@gmail.com>
2026-05-08 23:15:28 +0530

    feat/a branch is complete

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ function add(a, b) {
     return a + b;
 }
 
+function sub(a, b) {
+    if(a > b) throw new Error("No a must be greator than b");
+    return a - b;
+}
+
 add(2, 5)
 add(10, 23)
 add(40, 42)
+
+sub(10, 5)
